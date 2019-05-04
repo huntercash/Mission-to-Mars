e963a2d98e075634c645611696eae338c3ad3628
Hunter Cash <hmclaug@gmail.com>
2019-05-04 17:04:46 -0500

    switching to mac because flask..

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
+import os
 
 # Create an instance of Flask
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mission_to_mars")
 
 
 # Route to render index.html template using data from Mongo
@@ -17,14 +18,14 @@ def home():
     mars_web_data = mongo.db.collection.find_one()
 
     # Return template and data
-    return render_template("index.html", mars=mars_web_data)
+    return render_template(os.path.join('index.html'))#, mars = mars_web_data)
 
 
 # Route that will trigger the scrape function
 @app.route("/scrape_mars")
 def scrape():
 
-    # Run the scrape function
+    # Run the scrape functionS
     mars_data = scrape_mars.scrape_info()
 
     # Update the Mongo database using update and upsert=True
